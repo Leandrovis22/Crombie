02ccb44a337d9da12567d9e5e70842655fb5767c
Leandro <leandroviscolungo@gmail.com>
2023-07-31 18:44:51 -0300

    Comento lo que hice

--- basic-html-css-js/script.js
+++ basic-html-css-js/script.js
@@ -1,14 +1,21 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
+var contador=1
 
 function checkGuess() {
+    document.getElementById("valorvariable").innerHTML=contador
     console.log("randomNumber", randomNumber);
     let guess = parseInt(document.getElementById('guessInput').value);
     let result = document.getElementById('result');
+    contador++
 
-    if (guess === randomNumber) {
-        result.innerHTML = '¡Felicitaciones! ¡Adivinaste el número!';
-    } else if (guess < randomNumber) {
+    if (guess < 0){
+        result.innerHTML = 'No sirve, debe ser mayor a -1';
+    }else if (guess < randomNumber){
         result.innerHTML = 'Intenta con un número más alto.';
+    }else if(guess > 100){
+        result.innerHTML = 'No sirve, debe ser menor que 100';
+    }else if (guess === randomNumber) {
+        result.innerHTML = '¡Felicitaciones! ¡Adivinaste el número!';
     } else {
         result.innerHTML = 'Intenta con un número más bajo.';
     }
